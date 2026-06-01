fc981986b8c1051dd74a36e9b14d8627e29f3935
Zenaire <zaf.mytser@gmail.com>
2026-06-01 14:14:18 +0700

    Kalibrasi

--- resources/views/livewire/user/home.blade.php
+++ resources/views/livewire/user/home.blade.php
@@ -125,7 +125,7 @@
 
                             <div class="flex gap-3 text-sm shrink-0">
                                 <a href="/edit" class="text-zinc-400 hover:text-white transition">Edit</a>
-                                <button class="text-red-400 hover:text-red-300 transition">Delete</button>
+                                <button wire:click='destroy({{}})' class="text-red-400 hover:text-red-300 transition">Delete</button>
                             </div>
                         </div>
 
